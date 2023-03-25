7e0457b64aef1cf394394e6032d643f07bff0cf7
gdh <guodonghao@mail.ustc.edu.cn>
2023-03-25 23:13:22 +0800

    解决相对路径出错问题

--- 学习爬虫/ustc_img.py
+++ 学习爬虫/ustc_img.py
@@ -6,7 +6,7 @@ try:
     response=requests.get(url)
     print(response.status_code)
     image=response.content
-    with open('./学习爬虫/ustc_img.jpg','wb') as f:
+    with open('./ustc_img.jpg','wb') as f:
         f.write(image)
 except Exception as ex:
     print('----------ERROR----------',ex)
